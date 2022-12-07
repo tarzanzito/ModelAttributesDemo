af056981bb0fc2f02336b67ddd02f3568263f98c
tarzanzito <tarzanzito@gmail.com>
2022-12-07 23:18:49 +0000

    main discriptions

--- ModelAttributesDemo/Program.cs
+++ ModelAttributesDemo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
 
@@ -9,42 +10,43 @@ namespace ModelAttributesManager
         [STAThread]
         static void Main(string[] args)
         {
-            //////////////
-            //1-Inspect //
-            //////////////
+            /////////////////////////
+            //1-Inspect Attributes //
+            /////////////////////////
             
-            //1.1-instance
+            //1.1-Create instance
             var modelHelper = new ModelHelper<ModelExample>();
             modelHelper.InspectModel();
             //or
             //var instance = new ModelHelper<ModelExample>().InspectModelFields();
 
             //2-Show 'ModelClassAttribute' informations
-            var moldelHelperFieldInfoArray = modelHelper.ModelHelperFieldInfoArray;
-            
+           
             string modelId = modelHelper.ModelId;
             int modelVersion = modelHelper.ModelVersion;
             ModelClassAttributeType modelType = modelHelper.ModelType;
+            IEnumerable<ModelHelperFieldInfo> moldelHelperFieldInfoArray = modelHelper.ModelHelperFieldInfoArray;
             string className = nameof(ModelExample);
 
 
             //3-Write To Console
+            //3.1-Class Attributes
             System.Console.WriteLine("className=" + className);
             System.Console.WriteLine("modelId=" + modelId);
             System.Console.WriteLine("modelVersion=" + modelVersion.ToString());
             System.Console.WriteLine("modelType=" + modelType);
 
-      ////////////////////////      moldelHelperFieldInfoList.Add()
-            //4-Show 'ModelFieldAttribute' information (list fields)
+
+            //3.2-Field Attributes 'ModelFieldAttribute'
             foreach (ModelHelperFieldInfo item in moldelHelperFieldInfoArray)
             {
-                //41.1-Show 'ModelFieldAttribute' informations
+                //3.2.1-Show 'ModelFieldAttribute' informations
                 string id = item.Id;
                 int index = item.Index;
                 int size = item.Size;
                 MemberInfo info = item.MemberInfo;
 
-                //4.2-Write To Console
+                //3.2-2-Write To Console
                 System.Console.WriteLine("-------------------------");
                 System.Console.WriteLine("Id=" + id);
                 System.Console.WriteLine("Index=" + index.ToString());
@@ -54,7 +56,7 @@ namespace ModelAttributesManager
             }
 
             ///////////////////////////
-            //5-Set Values In Fields //
+            //4-Set Values In Fields //
             ///////////////////////////
             var newModel = modelHelper.GetNewModelInstance();
 
@@ -64,13 +66,13 @@ namespace ModelAttributesManager
             modelHelper.SetModelValueByAttributeId(newModel, "Field5", 30);
             modelHelper.SetModelValueByFieldName(newModel, "echo", 25);
 
-            //5.1-static examples
+            //4.1-static examples demo
             ModelExample newModel2 = ModelHelper<ModelExample>.GetNewModelInstanceS(); // REF002
-            //5.2-static but stupid (passing Type)
+            //4.2-static but stupid (passing Type)
             var newModel3 = ModelHelper<ModelExample>.GetNewModelInstanceS(typeof(ModelExample)); //REF003
 
             ////////////////////////////
-            //6-Get Attributes Fields //
+            //5-Get Attributes Fields //
             ////////////////////////////
             ModelHelperFieldInfo modelHelperFieldInfo1 = modelHelper.GetFieldAttributeById("Field5");
             ModelHelperFieldInfo modelHelperFieldInfo2 = modelHelper.GetFieldAttributeByName("echo");
